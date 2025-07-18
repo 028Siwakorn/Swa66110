4fa952a18a4eb078fbceab377175e38fed9d1349
Siwakorn Kleebmek <664259028@webmail.npru.ac.th>
2025-07-18 15:16:36 +0700

    SWA

---
+++ TemplateMethodDemo.java
@@ -0,0 +1,7 @@
+import th.ac.npru.se.sw.*;
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Generalization algorithm = new Realization();
+        algorithm.findSolution();
+    }
+}
